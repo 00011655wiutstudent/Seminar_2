50e225f0f0a292f3ee69849ec56bd4089e3f56c6
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-10-27 16:46:14 +0500

    Homowerk was aseier that I had expected

---
+++ homework.py
@@ -0,0 +1,37 @@
+def new_line():
+
+    print('*')
+
+def three_lines():
+
+    new_line()
+
+    new_line()
+
+    new_line()
+
+#create 9 lines of *
+def nine_lines():
+
+    three_lines()
+
+    three_lines()
+
+    three_lines()
+
+# create 25 lines of *
+def clear_screen():
+
+    nine_lines()
+
+    nine_lines()
+
+    three_lines()
+
+    three_lines()
+
+    new_line()
+
+clear_screen()
+
+
