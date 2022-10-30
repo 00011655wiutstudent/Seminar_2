e8ea188df9979c7572683ae923f396b06dd697f7
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-10-30 10:05:04 +0500

    branching

--- seminar_2_tasks.py
+++ seminar_2_tasks.py
@@ -1,16 +1,3 @@
-cw = input("What is your score form cw?")
-exam = input("Your mark on exam?")
-cw_mark = int(cw)
-exam_mark = int(exam)
-Final_mark = cw_mark*0.4+exam_mark*0.6
-print(Final_mark)
-
-check = Final_mark % 2
-
-if check != 0:
-    print("this is an odd number")
-else:
-    print("this is the even number")
 
 def my_function():
     inputt = input("yes or no")
@@ -28,3 +15,4 @@ def add(a, b):
     print(answer)
 
 add(9, 9)
+#I am trying branching
